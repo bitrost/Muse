17f5aa330d3497a36cd28e1a41c0e5547b89a9ef
lauren <lc28@hw.ac.uk>
2017-12-31 22:21:55 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
-//#include <gtkmm.h>
 #include <iostream>
 #include <gtkmm/application.h>
 
-//#include "SDL2/SDL.h"
-//#include "SDL2/SDL_mixer.h"
 #include "interface.h"
 #include "player.h"
-
-// To compile:
-// g++ main.cpp -o Muse -lSDL2 -lSDL2_mixer `pkg-config --cflags --libs gtkmm-3.0`
 
 int main(int argc, char **argv)
 {
@@ -21,7 +15,6 @@ int main(int argc, char **argv)
 
   player.initialize();
   player.load_music();
-  // Mix_PlayMusic(music, 1);
 
   // Shows the window and returns when it is closed.
   return app->run(window);
